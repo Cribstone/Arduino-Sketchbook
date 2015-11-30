8af8c5d9c24369e1c4bb090c1ab5a0fc6e803a3d
Andrew Jawitz <andrewjawitz@gmail.com>
2015-11-30 16:45:03 -0500

    Updated Libraries and added vanilla MQTT Button sketch

---
+++ HASSMQTTBUTTON/HASSMQTTBUTTON.ino
@@ -0,0 +1,110 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Wifi Connection
+const char* ssid = "YOUR SSID";
+const char* password = "SSID PASSWORD";
+
+// MQTT Server address
+const char* mqtt_server = "Your MQTT IP Address ";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup() {
+  
+    // prepare GPIO4
+  pinMode(4, OUTPUT);
+  digitalWrite(4, 1);
+
+  Serial.begin(9600);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void setup_wifi() {
+
+  delay(2);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  int val;
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    val = 1;
+    digitalWrite(4, val);
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+    val = 0;
+    digitalWrite(4, val);
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("/switch/relay/state", "connected");
+      // ... and resubscribe
+      client.subscribe("/switch/relay/switch");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, 75, "connected", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("/switch/relay/state", msg);
+  }
+}
