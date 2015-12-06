ff735998a9ab5bf9108b373a10b2cebf66231cb6
Andrew Jawitz <andrewjawitz@gmail.com>
2015-12-06 12:08:00 -0500

    Added first experiments with ESP8266

--- HASSMQTTBUTTON/HASSMQTTBUTTON.ino
+++ HASSMQTTBUTTON/HASSMQTTBUTTON.ino
@@ -2,11 +2,11 @@
 #include <PubSubClient.h>
 
 // Wifi Connection
-const char* ssid = "YOUR SSID";
-const char* password = "SSID PASSWORD";
+const char* ssid = "Tomato24";
+const char* password = "roosevelt";
 
 // MQTT Server address
-const char* mqtt_server = "Your MQTT IP Address ";
+const char* mqtt_server = "192.168.1.9";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -79,9 +79,9 @@ void reconnect() {
     if (client.connect("ESP8266Client")) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("/switch/relay/state", "connected");
+      client.publish("home/bedroom/switch1/set", "connected");
       // ... and resubscribe
-      client.subscribe("/switch/relay/switch");
+      client.subscribe("home/bedroom/switch1");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -105,6 +105,6 @@ void loop() {
     snprintf (msg, 75, "connected", value);
     Serial.print("Publish message: ");
     Serial.println(msg);
-    client.publish("/switch/relay/state", msg);
+    client.publish("home/bedroom/switch1", msg);
   }
 }
